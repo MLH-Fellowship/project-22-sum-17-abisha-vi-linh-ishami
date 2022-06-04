fb132f6f02ec9b0e33af6293e039c1892bc51d32
vilinh <vilinhlvu@gmail.com>
2022-06-04 15:29:44 -0700

    projects data in flask and html template

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,16 @@ load_dotenv()
 app = Flask(__name__)
 
 
+class Proj:
+    def __init__(self, name, descrip, git, demo) -> None:
+        self.name = name
+        self.descrip = descrip
+        self.git = git
+        self.demo = demo
+
+
 @app.route('/')
 def index():
-    return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"))
+    projs = [Proj("Proj 1", "Description of my proj 1!", "https://google.com/", "https://github.com/"), Proj("Proj 2", "This is my proj 2!!",
+                                                                                                             "https://github.com/", "https://github.com/"),  Proj("Proj 3", "Description of my proj 3", "https://github.com/", "https://github.com/")]
+    return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"), projects=projs)
