e65e54437e57fb1f726dc536adb66871e0b6f29b
ruliGIT <91031188+ruliGIT@users.noreply.github.com>
2022-06-24 01:20:18 -0400

    Adding a db part 1

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,20 @@
 import os
 from flask import Flask, render_template, request
 from dotenv import load_dotenv
+from peewee import *
 
 load_dotenv()
 app = Flask(__name__)
+
+#connect to MySQL db
+mydb = MySQLDatabase(os.getenv("MYSQL_DATABASE"),
+    user=os.getenv("MYSQL_USER"),
+    password=os.getenv("MYSQL_PASSWORD"),
+    host=os.getenv("MYSQL_HOST"),
+    port=3306
+)
+
+print(mydb)
 
 
 class Proj:
