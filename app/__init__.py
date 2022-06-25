f5effdd432d3d9b40ee91475f5f0510d9012b910
ruliGIT <91031188+ruliGIT@users.noreply.github.com>
2022-06-25 00:49:35 -0400

    Adding Database (part 2)
    - curl-test bash script
    - get, post endpoints

--- app/__init__.py
+++ app/__init__.py
@@ -2,9 +2,12 @@ import os
 from flask import Flask, render_template, request
 from dotenv import load_dotenv
 from peewee import *
+import datetime
+from playhouse.shortcuts import model_to_dict 
 
 load_dotenv()
 app = Flask(__name__)
+
 
 #connect to MySQL db
 mydb = MySQLDatabase(os.getenv("MYSQL_DATABASE"),
@@ -15,6 +18,18 @@ mydb = MySQLDatabase(os.getenv("MYSQL_DATABASE"),
 )
 
 print(mydb)
+
+class TimelinePost(Model):
+    name = CharField()
+    email = CharField()
+    content = TextField()
+    created_at = DateTimeField(default=datetime.datetime.now)
+
+    class Meta:
+        database = mydb
+
+mydb.connect()
+mydb.create_tables([TimelinePost])
 
 
 class Proj:
@@ -71,3 +86,28 @@ def index():
 @app.route('/hobbies')
 def hobbies():
     return render_template('hobbies.html', url=os.getenv("URL"), polaroids=pols)
+
+
+# create db timeline_post
+@app.route('/api/timeline_post', methods=['POST'])
+def post_time_line_post():
+    name = request.form['name']
+    email = request.form['email']
+    content = request.form['content']
+    timeline_post = TimelinePost.create(name=name, email=email, content=content)
+
+    return model_to_dict(timeline_post)
+
+# create a GET endpoint that retrieves all timeline posts ordered by created_at descending 
+# so the newest timeline posts are returned at the top
+@app.route('/api/timeline_post', methods=['GET'])
+def get_time_line_post():
+    return {
+        'timeline_posts': [
+            model_to_dict(p)
+            for p in 
+TimelinePost.select().order_by(TimelinePost.created_at.desc())
+        ]
+    }
+
+
