de7d1611548d9b50e2f4a2f21e0419a088a5e9c0
ruliGIT <91031188+ruliGIT@users.noreply.github.com>
2022-06-29 21:04:04 -0400

    Update to redeploy.sh (create service part 2)

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-tmux attach -d
 cd 22.SUM.17-IR-Portfolio-Website
 git fetch && git reset origin/main --hard
 source python3-virtualenv/bin/activate
 pip install -r requirements.txt
-tmux new | cd app | export FLASK_APP=__init__.py | flask run --host=0.0.0.0
+systemctl daemon-reload
+systemctl restart myportfolio
