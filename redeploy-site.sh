d6e36699413b58e47bb9b50afe9b32c29b4a6b19
ruliGIT <91031188+ruliGIT@users.noreply.github.com>
2022-07-07 22:03:47 -0400

    Update to redeploy-site
    - changing steps to match with Docker

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 cd 22.SUM.17-IR-Portfolio-Website
 git fetch && git reset origin/main --hard
-source python3-virtualenv/bin/activate
-pip install -r requirements.txt
-systemctl daemon-reload
-systemctl restart myportfolio
+docker compose -f docker-compose.prod.yml down
+docker compose -f docker-compose.prod.yml up -d --build
